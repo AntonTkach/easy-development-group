c284605604143dc2017c5c5d05ca2b11c7988620
Anton Tkach <dekerter@gmail.com>
2018-03-03 12:49:18 +0200

    A basic working server

--- backEndDataStorage/WebServer/src/webserver/WebServer.java
+++ backEndDataStorage/WebServer/src/webserver/WebServer.java
@@ -5,15 +5,18 @@
  */
 package webserver;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 /**
  *
@@ -24,132 +27,38 @@ public class WebServer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int port = 9000;
-HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-System.out.println("server started at " + port);
-server.createContext("/", new RootHandler());
-server.createContext("/echoHeader", new EchoHeaderHandler());
-server.createContext("/echoGet", new EchoGetHandler());
-server.createContext("/echoPost", new EchoPostHandler());
-server.setExecutor(null);
-server.start();
+    public static void main(String[] args) throws IOException {
+        int socketNumber = 8080;
+        InetSocketAddress addr = new InetSocketAddress(socketNumber);
+        HttpServer server = HttpServer.create(addr, 0);
+
+        server.createContext("/", new DefaultHandler());
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
+        System.out.println("Server is listening on port " + socketNumber);
     }
-    
 }
 
-public class RootHandler implements HttpHandler {
+class DefaultHandler implements HttpHandler {
 
-         @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String requestMethod = exchange.getRequestMethod();
+        if (requestMethod.equalsIgnoreCase("GET")) {
+            Headers responseHeaders = exchange.getResponseHeaders();
+            responseHeaders.set("Content-Type", "text/plain");
+            exchange.sendResponseHeaders(200, 0);
 
-         public void handle(HttpExchange he) throws IOException {
-                 String response = "<h1>Server start success "
-                         + "if you see this message</h1>" + "<h1>Port: " + port + "</h1>";
-                 he.sendResponseHeaders(200, response.length());
-                 OutputStream os = he.getResponseBody();
-                 os.write(response.getBytes());
-                 os.close();
-         }
+            OutputStream responseBody = exchange.getResponseBody();
+            Headers requestHeaders = exchange.getRequestHeaders();
+            Set<String> keySet = requestHeaders.keySet();
+            Iterator<String> iter = keySet.iterator();
+            while (iter.hasNext()) {
+                String key = iter.next();
+                List values = requestHeaders.get(key);
+                String s = key + " = " + values.toString() + "\n";
+                responseBody.write(s.getBytes());
+            }
+            responseBody.close();
+        }
+    }
 }
-
-public class EchoHeaderHandler implements HttpHandler {
-
-         @Override
-         public void handle(HttpExchange he) throws IOException {
-                 Headers headers = he.getRequestHeaders();
-                 Set<Map.Entry<String, List<String>>> entries = headers.entrySet();
-                 String response = "";
-                 for (Map.Entry<String, List<String>> entry : entries)
-                          response += entry.toString() + "\n";
-                 he.sendResponseHeaders(200, response.length());
-                 OutputStream os = he.getResponseBody();
-                 os.write(response.toString().getBytes());
-                 os.close();
-         }}
-
-public class EchoGetHandler implements HttpHandler {
-
-         @Override
-
-         public void handle(HttpExchange he) throws IOException {
-                 // parse request
-                 Map<String, Object> parameters = new HashMap<String, Object>();
-                 URI requestedUri = he.getRequestURI();
-                 String query = requestedUri.getRawQuery();
-                 parseQuery(query, parameters);
-
-                 // send response
-                 String response = "";
-                 for (String key : parameters.keySet())
-                          response += key + " = " + parameters.get(key) + "\n";
-                 he.sendResponseHeaders(200, response.length());
-                 OutputStream os = he.getResponseBody();
-                 os.write(response.toString().getBytes());
-
-                 s.close();
-         }
-}
-
-
-public class EchoPostHandler implements HttpHandler {
-
-         @Override
-
-         public void handle(HttpExchange he) throws IOException {
-                 // parse request
-                 Map<String, Object> parameters = new HashMap<String, Object>();
-                 InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
-                 BufferedReader br = new BufferedReader(isr);
-                 String query = br.readLine();
-                 parseQuery(query, parameters);
-
-                 // send response
-                 String response = "";
-                 for (String key : parameters.keySet())
-                          response += key + " = " + parameters.get(key) + "\n";
-                 he.sendResponseHeaders(200, response.length());
-                 OutputStream os = he.getResponseBody();
-                 os.write(response.toString().getBytes());
-                 os.close();
-         }
-         
-         public static void parseQuery(String query, Map<String, 
-	Object> parameters) throws UnsupportedEncodingException {
-
-         if (query != null) {
-                 String pairs[] = query.split("[&]");
-                 for (String pair : pairs) {
-                          String param[] = pair.split("[=]");
-                          String key = null;
-                          String value = null;
-                          if (param.length > 0) {
-                          key = URLDecoder.decode(param[0], 
-                          	System.getProperty("file.encoding"));
-                          }
-
-                          if (param.length > 1) {
-                                   value = URLDecoder.decode(param[1], 
-                                   System.getProperty("file.encoding"));
-                          }
-
-                          if (parameters.containsKey(key)) {
-                                   Object obj = parameters.get(key);
-                                   if (obj instanceof List<?>) {
-                                            List<String> values = (List<String>) obj;
-                                            values.add(value);
-
-                                   } else if (obj instanceof String) {
-                                            List<String> values = new ArrayList<String>();
-                                            values.add((String) obj);
-                                            values.add(value);
-                                            parameters.put(key, values);
-                                   }
-                          } else {
-                                   parameters.put(key, value);
-                          }
-                 }
-         }
-}
-}
-
-

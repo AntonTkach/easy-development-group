0b1039ffba19f62a8726a35ac3ad0aa48e1b2707
Anton Tkach <dekerter@gmail.com>
2018-03-04 01:19:41 +0200

    Polishing and tidying up

--- backEndDataStorage/WebServer/src/webserver/WebServer.java
+++ backEndDataStorage/WebServer/src/webserver/WebServer.java
@@ -1,26 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package webserver;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.Executors;
 
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -28,15 +11,10 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Scanner;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -49,16 +27,19 @@ public class WebServer {
      */
     public static void main(String[] args) throws IOException {
         int socketNumber = 8080;
+        String defaultHandlerAdress="/"; // this handles both of the requests. Could be split
+        String getHandlerAdress="/get"; 
+        String postHandlerAdress="/post";
         InetSocketAddress addr = new InetSocketAddress(socketNumber);
         HttpServer server = HttpServer.create(addr, 0);
 
-        server.createContext("/", new DefaultHandler());
+        server.createContext(defaultHandlerAdress, new DefaultHandler());
         server.setExecutor(Executors.newCachedThreadPool());
         server.start();
         System.out.println("Server is listening on port " + socketNumber);
-        System.out.println("Go to localhost:" + socketNumber
-                + " or 127.0.0.1:" + socketNumber
-                + " \n OR to 192.168.1.108:" + socketNumber);
+        System.out.println("Go to \n localhost:" + socketNumber
+                + " \n 127.0.0.1:" + socketNumber
+                + " \n LAN IP 192.168.1.108:" + socketNumber);
     }
 }
 
@@ -76,7 +57,8 @@ class DefaultHandler implements HttpHandler {
 //            Set<String> keySet = requestHeaders.keySet();
 //            Iterator<String> iter = keySet.iterator();
 
-            String output = "HELLO WORLD";
+            String output = "HELLO WORLD"
+                    + "\n\n\n This is a GET response \n\n\n";
 //            while (iter.hasNext()) {
 //                String key = iter.next();
 //                List values = requestHeaders.get(key);
@@ -110,44 +92,18 @@ class DefaultHandler implements HttpHandler {
             // RESPONSE Headers
             // Send RESPONSE Headers
 //            extends.sendResponseHeaders(HttpURLConnection.HTTP_OK, contentLength);
+            // TODO: WTF isn't it working?
+            
             // RESPONSE Body
 //            OutputStream os = exchange.getResponseBody();
 //
 //            os.write(data);
             responseBody.write(data);
-            
-
-//            String output = "This is a POST response \n"
-//                    + os.toString() + "\n"
-//                    + length + "\n"
-//                    + data.toString() + "\n"
-//                    + entries.toString() + "\n"
-//                    + contentLength + "\n";
+           
             String output = "\n\n\nThis is a POST response \n\n\n";
-//            output+=HttpServletRequest.getHeaderNames();
-
-//            while (iter.hasNext()) {
-//                String key = iter.next();
-//                List values = requestHeaders.get(key);
-//                String s = key + " = " + values.toString() + "\n";
-//                responseBody.write(s.getBytes());
-//            }
+            
             responseBody.write(output.getBytes());
             responseBody.close();
         }
     }
-
-//    public boolean isInQuery(HttpServletRequest request, String key) {
-//        String query = request.getQueryString();
-//        System.out.println(query);
-//        String[] nameValuePairs = query.split("&");
-//        System.out.println(nameValuePairs.toString());
-//        for (String nameValuePair : nameValuePairs) {
-//            if (nameValuePair.startsWith(key + "=")) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
 }

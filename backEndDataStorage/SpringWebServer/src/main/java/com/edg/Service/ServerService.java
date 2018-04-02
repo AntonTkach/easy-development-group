f61b863ea0e21314e636f8d735bbe7eaa41af09d
Anton Tkach <dekerter@gmail.com>
2018-04-02 18:25:10 +0300

    Cleaned System print statements

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Service/ServerService.java
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Service/ServerService.java
@@ -25,14 +25,12 @@ public class ServerService {
     public String getUserPass(String jsonStringed){
         JSONObject jsonObject = new JSONObject(jsonStringed);
         String userName = jsonObject.getString("userName");
-
         String sqlQuery = "SELECT passwordHash " +
                 "FROM Users " +
                 "WHERE (userName=\"" + userName + "\")";
 
         String jsonString = getDataFromDB(sqlQuery);
         JSONArray jsonArray = new JSONArray(jsonString);
-
         return jsonArray.getJSONObject(0).getString("passwordHash");
     }
 

d30c90dbd8e37d75ec26460f8794343d17b68574
sozakh <sozahharenko@gmail.com>
2018-04-09 17:33:27 +0300

    test begining
    getAllTasksTest

---
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Tests/ServerControllerTest.java
@@ -0,0 +1,67 @@
+package com.edg.Tests;
+
+
+import com.edg.Controller.ServerController;
+import com.edg.Service.ServerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = ServerController.class)
+public class ServerControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @MockBean
+    private ServerController serverServiceMock;
+    
+    @Before
+    public void saveTaskInDB() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+   /* @Test
+    public void saveTaskInDBTest() throws Exception {
+        assertThat(this.serverRepository).isNotNull();
+        mockMvc.perform(ServerServiceRequestBuilders.post("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(ServerServiceResultMatchers.view().name("register"))
+                .andExpect(content().string(Matchers.containsString("Spring Framework Guru")))
+                .andDo(print());
+    }
+    
+    */
+    
+     @Test
+    public void getAllTasksInDB() throws Exception {
+        assertThat(this.serverServiceMock).isNotNull();
+        mockMvc.perform(MockMvcRequestBuilders.get("/gettasks"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("products"))
+                .andExpect(MockMvcResultMatchers.view().name("products"))
+                .andDo(print());
+    }
+    
+}

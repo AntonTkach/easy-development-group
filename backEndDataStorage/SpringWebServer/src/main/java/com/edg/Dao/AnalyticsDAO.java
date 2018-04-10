36844301b1a151634ffda4687da0c44c91ca55f8
Anton Tkach <dekerter@gmail.com>
2018-04-11 00:50:44 +0300

    Moved AnalyticsDAO and AnalyticsDAOImpl to separate branch

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.edg.Dao;
-
-public interface AnalyticsDAO{
-    //void saveUserInDB(String sqlQuery);
-    void analyseSaveTaskInDB(String sqlQuery);
-    void analyseUpdateTaskInDB(String sqlQuery);
-    void analyseDeleteTaskInDB(String sqlQuery);
-    void analyseSavePomodoroInDB(String sqlQuery);
-}

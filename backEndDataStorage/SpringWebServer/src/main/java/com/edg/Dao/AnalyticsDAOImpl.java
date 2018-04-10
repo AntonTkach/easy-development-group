44557c665d1ce6364e7dd7a916b580013e709881
Margarita Sidorenko <masido@ttu.ee>
2018-04-11 00:28:26 +0300

    Some sql added

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAOImpl.java
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAOImpl.java
@@ -62,4 +62,80 @@ public class AnalyticsDAOImpl implements AnalyticsDAO {
             System.out.println(e.getMessage());
         }
     }
+
+    /* *
+
+        TODO: SQL FOR Analytics. REWATCH FOR NEW LOGIC.
+
+        select * from  Pomodoros
+        inner join Users on Users.userID = Pomodoros.userID
+        inner join Tasks on Tasks.taskID = Pomodoros.taskID
+
+        --1.	Total of eaten pomodoros
+        select count(pomodoroID),Users.userName from  Pomodoros
+        inner join Users on Users.userID = Pomodoros.userID
+        --where Users.userName = "Nomad"
+        group by Users.userName
+
+
+        --2.	Total tasks done
+        select count(taskID),userName from Tasks
+        inner join Users on Users.userID = Tasks.userID
+        where Tasks.isCompleted = 1 and Users.userName = "Nomad"
+        group by userName
+
+
+        --3.	Total tasks added
+        select count(taskID),userName from Tasks
+        inner join Users on Users.userID = Tasks.userID
+        --where Users.userName = "Nomad"
+        group by userName
+
+
+        --4.	Total possible rest had
+        select sum(Pomodoros.restTime),Users.userName from  Pomodoros
+        inner join Users on Users.userID = Pomodoros.userID
+        where Users.userName = "Nomad"
+        group by Users.userName
+
+
+        --5.	Total time worked
+        select sum(Pomodoros.workTime),Users.userName from  Pomodoros
+        inner join Users on Users.userID = Pomodoros.userID
+        where Users.userName = "Nomad"
+        group by Users.userName
+
+
+        --6.	Total time in general on work/rest
+        select sum(Pomodoros.workTime)+sum(Pomodoros.restTime),Users.userName from  Pomodoros
+        inner join Users on Users.userID = Pomodoros.userID
+        where Users.userName = "Nomad"
+        group by Users.userName
+
+
+        --7.	Average # of pomodoros per task
+        select count(pomodoroID)*1.0/(
+        select count(taskID) from Tasks
+        inner join Users on Users.userID = Tasks.userID
+        where Tasks.isCompleted = 1 and Users.userName = "Sam"
+        group by userName
+        ) as taskCount from  Pomodoros
+        inner join Users on Users.userID = Pomodoros.userID
+        where Users.userName = "Sam"
+        group by Users.userName
+
+
+        --8.	Average # of pomodoros per day/week
+
+
+        --9.	Average # of tasks per day/week
+
+
+        --10.	Average # of tasks done per day/week
+
+
+
+    * */
+
+
 }

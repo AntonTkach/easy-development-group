c955fc09ac0e696faa39e2c44dd7cda9ba439ba0
Margarita Sidorenko <masido@ttu.ee>
2018-04-10 23:48:02 +0300

    Prepared all methods in Interface for implementation

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAO.java
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAO.java
@@ -1,10 +1,9 @@
 package com.edg.Dao;
 
 public interface AnalyticsDAO{
-    //void saveUserInDB(String sqlQuery, String userName, String passwordHash);
-    void analyseSaveTaskInDB(String sqlQuery, String taskName, String taskBody, String userID, boolean isCompleted, long timestamp);
-    void analyseUpdateTaskInDB(String sqlQuery, String taskName, String taskBody, boolean isCompleted, int taskID);
-    void analyseDeleteTaskInDB(String sqlQuery, int taskID);
+    //void saveUserInDB(String sqlQuery);
+    void analyseSaveTaskInDB(String sqlQuery);
+    void analyseUpdateTaskInDB(String sqlQuery);
+    void analyseDeleteTaskInDB(String sqlQuery);
     void analyseSavePomodoroInDB(String sqlQuery);
-
 }

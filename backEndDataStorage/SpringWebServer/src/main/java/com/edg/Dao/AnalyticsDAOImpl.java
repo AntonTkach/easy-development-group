6610d49e0ff559592b8dfb4297394a214ad6e907
Margarita Sidorenko <masido@ttu.ee>
2018-04-10 23:55:10 +0300

    Analytics class with Implementation

---
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAOImpl.java
@@ -0,0 +1,12 @@
+package com.edg.Dao;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+
+@Repository
+public class AnalyticsDAOImpl implements ServerDao {
+
+}

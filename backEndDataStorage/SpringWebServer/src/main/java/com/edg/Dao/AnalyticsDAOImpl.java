7a5aaa33aec81e85ed20c37139787a48217c2f5e
Margarita Sidorenko <masido@ttu.ee>
2018-04-11 00:17:24 +0300

    analyseSaveTaskInDB Implementation

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAOImpl.java
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Dao/AnalyticsDAOImpl.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Repository;
 import java.sql.*;
 
 @Repository
-public class AnalyticsDAOImpl implements ServerDao {
-
+public class AnalyticsDAOImpl implements AnalyticsDAO {
+    public void analyseSaveTaskInDB(String sqlQuery){
+        try (Connection conn = this.connect();
+             PreparedStatement preparedStatement = conn.prepareStatement(sqlQuery)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }

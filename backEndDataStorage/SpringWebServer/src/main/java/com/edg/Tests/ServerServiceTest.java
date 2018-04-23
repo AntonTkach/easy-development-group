60a322296cc3f53b1a2c7317ae32ed6304d0a610
sozakh <sozahharenko@gmail.com>
2018-04-23 22:19:17 +0300

    @WebAppConfiguration

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Tests/ServerServiceTest.java
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Tests/ServerServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = ServerService.class)
+@WebAppConfiguration
 public class ServerServiceTest {
      private MockMvc mockMvc;
      @Autowired

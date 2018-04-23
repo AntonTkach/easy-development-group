4089f323296fca44d6a99b593b7df8ab73550936
sozakh <sozahharenko@gmail.com>
2018-04-23 22:00:47 +0300

    RunWith

--- backEndDataStorage/SpringWebServer/src/main/java/com/edg/Tests/ServerDaoTest.java
+++ backEndDataStorage/SpringWebServer/src/main/java/com/edg/Tests/ServerDaoTest.java
@@ -5,10 +5,26 @@
  */
 package com.edg.Tests;
 
-/**
- *
- * @author София
- */
+import com.edg.Dao.ServerDaoImpl;
+import com.edg.Service.ServerService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+ 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
 public class ServerDaoTest {
     
 }
